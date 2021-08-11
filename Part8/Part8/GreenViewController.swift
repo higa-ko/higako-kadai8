278600d81e5ed622679b7505a7ab530aa0918ff2
higa-ko <85394813+higa-ko@users.noreply.github.com>
2021-08-11 20:47:09 +0900

    Add files via upload

---
+++ Part8/Part8/GreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GreenViewController.swift
+//  Part8
+//
+//  Created by 葡萄酒 on 2021/08/11.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+    
+    @IBOutlet private weak var valueLabel: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+    
+    let delegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBAction func actionSlider(_ sender: UISlider) {
+        
+        let value = slider.value
+        
+        valueLabel.text = String(value)
+        delegate.shareValue = value
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        guard let value = delegate.shareValue else {
+            return
+        }
+        
+        valueLabel.text = String(value)
+        slider.value = value
+    }
+}
